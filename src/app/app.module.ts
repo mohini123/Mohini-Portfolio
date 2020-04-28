28abab9411c1508cf94d3c650c50917980d745ef
mohini123 <mohinimulik5@gmail.com>
2020-04-28 16:20:18 -0700

    updated toggle switch

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { UiSwitchModule } from 'ngx-toggle-switch';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -9,7 +8,6 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    UiSwitchModule
   ],
   providers: [],
   bootstrap: [AppComponent]
